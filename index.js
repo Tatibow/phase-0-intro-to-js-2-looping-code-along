71eab6fc62fe14f995bac9a2ae071dc49218cfb9
Tatiana <Bowmantatiana2@gmail.com>
2022-12-13 15:06:32 -0800

    commit tree 4b46171445d49031581e7f453fde91c68d2b2595

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, event) {
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+}
+
+// console.log(writeCards(names, "suprise"))
+
+
+function countDown(number) {
+    while(number >= 0) {
+        console.log(number--);
+    }
+}
+
+
